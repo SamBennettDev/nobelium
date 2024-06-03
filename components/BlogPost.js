08d1fc0e925a56b150993977b25a5dd690d4b073
Sam Bennett <sambennettdev@gmail.com>
2024-06-02 21:13:37 -0600

    support for images

--- components/BlogPost.js
+++ components/BlogPost.js
@@ -1,13 +1,28 @@
 import FormattedDate from "@/components/FormattedDate";
 import { useConfig } from "@/lib/config";
 import Link from "next/link";
+import Image from "next/image";
+import Post from "./Post";
 
 const BlogPost = ({ post }) => {
   const BLOG = useConfig();
 
   return (
     <Link href={`${BLOG.path}/${post.slug}`}>
-      <article key={post.id} className="mb-6 md:mb-8">
+      <article key={post.id} className="mb-8 md:mb-10">
+        {post.Image ? (
+          <>
+            <Image
+              className="rounded-3xl object-cover mb-6"
+              src={post.Image}
+              alt="cool"
+              width="720"
+              height="480"
+            />
+          </>
+        ) : (
+          ""
+        )}
         <header className="flex flex-col justify-between md:flex-row md:items-baseline">
           <h2 className="text-lg md:text-xl font-medium mb-2 cursor-pointer text-black dark:text-gray-100">
             {post.title}
